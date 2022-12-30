063ea10282f4d192dbab32cffbcd521a293b4d59
kosnoa <f4less0152@gmail.com>
2022-12-30 16:48:12 -0500

    Seungwoo Ko Week 0 (WIP)

---
+++ Week 00/Seungwoo/10699.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("2022-12-31\n");
+    return 0;
+}
---
+++ Week 00/Seungwoo/7287.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("2\n");
+    printf("c_is_good");
+    return 0;
+}
