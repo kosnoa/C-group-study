4a15e4cba52aba8efe1cac7f3a9164468ed10cb9
jihoson0212 <Sonjh5153@gmail.com>
2023-01-03 17:24:48 -0500

    Week 0 Clear

---
+++ Week 00/Jiho/1008.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d %d", &a,&b);
+    printf("%.10lf\n", (double)a/b);
+    return 0;
+}
---
+++ Week 00/Jiho/10699.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("2023-01-04\n");
+    return 0;
+}
